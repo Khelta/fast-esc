eda1ff5c4d291866eb5a08746d1c9f4e8bce62f9
Khelta <jonas-helm@gmx.de>
2025-08-06 23:30:07 +0200

    Renamed router file names to include router

--- src/main.py
+++ src/main.py
@@ -1,19 +1,17 @@
 from fastapi import FastAPI
 
 from src.database import create_db_and_tables
-from src.routers.dataimport import contest, country, song
-from src.routers.models import person
-from src.seed import seed_database
+from src.routers.dataimport import contest_import_router, country_import_router, song_import_router
+from src.routers.models import person_router
 
 app = FastAPI()
 
-app.include_router(country.router)
-app.include_router(contest.router)
-app.include_router(song.router)
-app.include_router(person.router)
+app.include_router(contest_import_router.router)
+app.include_router(country_import_router.router)
+app.include_router(person_router.router)
+app.include_router(song_import_router.router)
 
 
 @app.on_event("startup")
 def on_startup():
     create_db_and_tables()
-    seed_database()

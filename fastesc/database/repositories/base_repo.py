20a95d11c30aede3790db25b95ba5ab0d699534f
Khelta <jonas-helm@gmx.de>
2025-08-14 13:20:24 +0200

    Fix: base_repo filter signature now correct

--- fastesc/database/repositories/base_repo.py
+++ fastesc/database/repositories/base_repo.py
@@ -38,12 +38,16 @@ class DatabaseRepository(Generic[Model]):
 
     async def filter(
             self,
+            *expressions: BinaryExpression,
             offset: int = 0,
             limit: int = None,
-            *expressions: BinaryExpression,
+
     ) -> list[Model]:
         query = select(self.model)
-        query = query.offset(offset).limit(limit)
+        if limit:
+            query = query.limit(limit)
+        if offset:
+            query = query.offset(offset)
         if expressions:
             query = query.where(*expressions)
         return list(await self.session.scalars(query))

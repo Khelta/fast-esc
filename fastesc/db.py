a802b1b5ac0e8accdbfb97b589448df93632e229
Khelta <jonas-helm@gmx.de>
2025-08-18 16:14:19 +0200

    Fix: Loading of .env file in root dir

--- fastesc/db.py
+++ fastesc/db.py
@@ -9,9 +9,10 @@ from sqlalchemy.ext.asyncio import (
     create_async_engine,
 )
 
+base_dir = os.path.dirname(__file__)
 env_paths = [
     os.path.join("/etc/secrets", ".env"),
-    os.path.join(os.getcwd(), ".env"),
+    os.path.join(base_dir, "../.env"),
 ]
 
 env_path = next((path for path in env_paths if os.path.exists(path)), None)
@@ -34,7 +35,7 @@ DATABASE_URL = "postgresql+asyncpg://{}:{}@{}:{}/{}".format(DATABASE_USERNAME,
 
 
 async def get_db_session() -> AsyncGenerator[AsyncSession, None]:
-    engine = create_async_engine(DATABASE_URL)
+    engine = create_async_engine(DATABASE_URL, echo=True)
     factory = async_sessionmaker(engine)
     async with factory() as session:
         try:

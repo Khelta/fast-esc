2f397f56cd454081caba72e43caccf472f5d41e9
Khelta <jonas-helm@gmx.de>
2025-09-03 18:30:30 +0200

    Handled empty text string in song text API

--- fastesc/api/models/models.py
+++ fastesc/api/models/models.py
@@ -115,6 +115,8 @@ class ParticipationPublic(ParticipationBase):
 
 
 class SongPublic(SongBase):
+    text: str
+
     artist: ArtistBase
     participations: List[ParticipationPublic]
 
--- fastesc/api/routers/models/song_text_router.py
+++ fastesc/api/routers/models/song_text_router.py
@@ -1,8 +1,9 @@
 from typing import Annotated, Optional
 
-from fastapi import APIRouter, Depends, Query
+from fastapi import APIRouter, Depends, Query, HTTPException
 
 from fastesc.api.dependencies import get_repository
+from fastesc.api.models.errors import ErrorResponse
 from fastesc.api.models.models import SongPublic, SongTextPublic
 from fastesc.database.models.models import Song as DB_Song
 from fastesc.database.repositories.base_repo import DatabaseRepository
@@ -15,13 +16,28 @@ SongRepository = Annotated[
 ]
 
 
-@router.get("", response_model=SongTextPublic)
+@router.get("",
+            response_model=SongTextPublic,
+            responses={
+                400: {
+                    "model": ErrorResponse,
+                    "description": "Text cannot be empty",
+                    "content": {
+                        "application/json": {
+                            "example": {"detail": "Text cannot be empty"}
+                        }
+                    },
+                }
+            }, )
 async def find_in_songtext(repository: SongRepository,
                            text: str,
                            case_sensitive: bool = False,
                            whole_word: bool = True,
                            offset: int = Query(0, ge=0),
                            limit: Optional[int] = Query(0, ge=0)) -> SongTextPublic:
+    if text == "":
+        raise HTTPException(status_code=400, detail=f"Text cannot be empty")
+
     songs = await repository.get_text_in_column("text",
                                                 text,
                                                 case_sensitive=case_sensitive,
